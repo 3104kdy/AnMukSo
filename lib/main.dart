8271253afee14af0d9324b180a3e05b4d6847564
3104kdy <3104kdy@naver.com>
2021-07-19 01:21:57 +0900

    [UPDATE] Change app bar title

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("STANDARD"),
+        title: Text("STANDARD_ in JUDA"),
       ),
       body: StreamBuilder(
         stream: FirebaseFirestore.instance.collection('test').doc('data').snapshots(),
